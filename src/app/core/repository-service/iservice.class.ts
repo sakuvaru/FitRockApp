c46a351281c85b86346972e6442c75606a719f8c
Enngage <ennspassion@gmail.com>
2017-05-20 11:26:57 +0200

    Updates IService

--- src/app/core/repository-service/iservice.class.ts
+++ src/app/core/repository-service/iservice.class.ts
@@ -2,23 +2,27 @@ import { IItem } from '../../repository/iitem.class';
 import { IOption } from '../../repository/ioption.class';
 import { Observable } from 'rxjs/Observable';
 
-export interface IService<T extends IItem> {
+export interface IService<TItem extends IItem> {
 
     type: string
 
-    getAll(options?: IOption[]): Observable<T[]>;
+    // each service class needs to be responsible for creating proper type of objects 
+    // because returning 'item as TItem' will not convert actual object to type
+    createEmptyItem<TItem extends IItem>(): TItem;
 
-    getByCodename(codename: string, options?: IOption[]): Observable<T>;
+    getAll(options?: IOption[]): Observable<TItem[]>;
 
-    getByGuid(guid: string, options?: IOption[]): Observable<T>;
+    getByCodename(codename: string, options?: IOption[]): Observable<TItem>;
 
-    getById(id: number, options?: IOption[]): Observable<T>;
+    getByGuid(guid: string, options?: IOption[]): Observable<TItem>;
 
-    edit(obj: T): Observable<T>;
+    getById(id: number, options?: IOption[]): Observable<TItem>;
+
+    edit(obj: TItem): Observable<TItem>;
 
     delete(id: number): Observable<boolean>;
 
-    create(obj: T): Observable<T>;
+    create(obj: TItem): Observable<TItem>;
     
     delete(id: number): Observable<boolean>
 }
