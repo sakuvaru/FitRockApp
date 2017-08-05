8526316a6f15e39f5f1883db223acf46d4e8f5b9
Enngage <ennspassion@gmail.com>
2017-08-05 14:20:51 +0200

    Comments data table

--- src/web-components/data-table/data-table-field.class.ts
+++ src/web-components/data-table/data-table-field.class.ts
@@ -1,13 +1,40 @@
 import { AlignEnum } from './align-enum';
 
 export class DataTableField<T> {
-    // required properties
-    public label: string;
+
+    /**
+     * Value of the field
+     */
     public value: (item: T) => string;
 
     // optional properties
+    /**
+     * translated value of 'labelKey' (if provided) is stored here
+     */
+    public label?: string;
+
+    /**
+     * Label key -> used to translate label
+     */
+    public labelKey?: string;
+
+    /**
+     * Flex width
+     */
     public flex?: number;
+
+    /**
+     * Indicates if field is subtle (smaller + faded color)
+     */
     public isSubtle?: boolean;
+
+    /**
+     * Align of text in field
+     */
     public align?: AlignEnum;
+
+    /**
+     * Indicates if field will be hidden on small screens
+     */
     public hideOnSmallScreens?: boolean = false;
 }   
--- src/web-components/data-table/data-table.config.ts
+++ src/web-components/data-table/data-table.config.ts
@@ -5,27 +5,102 @@ import { Guid, StringHelper } from '../../lib/utilities';
 
 export class DataTableConfig<TItem extends IItem> {
 
+    /**
+     * Fields in the data table
+     */
     public fields: DataTableField<any>[];
+
+    /**
+     * Indicates if data table is wrapped in a material design card
+     */
     public wrapInCard: boolean = true;
+
+    /**
+     * Indicate if header with columns is shown. All fields need to configure their labels if enabled
+     */
     public showHeader: boolean = false;
+
+    /**
+     * Indicates if search is shown
+     */
     public showSearch: boolean = true;
+
+    /**
+     * Key of translation that is shown when no search results are found
+     */
     public searchNoItemsTextKey: string = 'webComponents.dataTable.noSearchResultsText'
+
+    /**
+     * Key of translation that is shown when data table contains 0 items
+     */
     public noItemsTextKey: string = 'webComponents.dataTable.noResultsText';
+
+    /**
+     * Indicates if filter with all items is shown
+     */
     public showAllFilter: boolean = false;
+
+    /**
+     * Indicates if the last state of data table is stored in local storage
+     * Search, page & active filter is stored if this is enabled
+     */
     public saveLastFilter: boolean = true;
 
+    /**
+     * Method that is used to get observable out of loadQuery. 
+     * Usually this should include 'takeUntil(this.ngUnsubscribe)' to ensure
+     * that requests are cancelled if they are not required (e.g. after destroying component)
+     */
     public loadResolver: (query: MultipleItemQuery<TItem>) => Observable<ResponseMultiple<TItem>>;
+
+    /**
+     * Used to specify query that loads items
+     */
     public loadQuery: (searchTerm: string) => MultipleItemQuery<TItem>;
+
+    /**
+     * On click handled
+     */
     public onClick: (item: TItem) => void;
+
+    /**
+     * If set, icon with with given function will be resolved, return name of the icon
+     */
     public iconResolver: (item: TItem) => string;
+
+    /**
+     * If set, avatar with given fucntion will be resolved - return URL of the image
+     */
     public avatarUrlResolver: (item: TItem) => string;
+
+    /**
+     * Handler executed after items are loaded
+     */
     public onAfterLoad: () => void;
+
+    /**
+     * Handler executed before items are loaded
+     */
     public onBeforeLoad: () => void;
 
+    /**
+     * Set of dynamic filters (do not combine with static filters)
+     */
     public dynamicFilters: <TFilter extends Filter<IItem>>(searchTerm: string) => Observable<Filter<IItem>[]>;
+
+    /**
+     * Set of static filters (do not combine with dynamic filters)
+     */
     public staticFilters: Filter<TItem>[] = [];
 
+    /**
+     * Configuratino of pager
+     */
     public pagerConfig: PagerConfig = new PagerConfig();
+
+    /**
+     * Configuration of selectable data table
+     */
     public selectableConfig: SelectableConfig<TItem>;
 
     constructor(
@@ -65,6 +140,9 @@ export class DataTableConfig<TItem extends IItem> {
         return !(!this.onClick);
     }
 
+    /**
+     * Gets unique hash of the configuration of data table
+     */
     getHash(): number{
         var allProperties = Object.getOwnPropertyNames(this);
         var fullText = '';
