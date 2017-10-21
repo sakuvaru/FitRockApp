5fd616365db5d64527d5141ef24b0065b482c4ed
Enngage <ennspassion@gmail.com>
2017-10-21 15:48:08 +0200

    fixes invalid validation of minimum number values

--- src/web-components/dynamic-form/dynamic-form-question.component.ts
+++ src/web-components/dynamic-form/dynamic-form-question.component.ts
@@ -232,24 +232,25 @@ export class DynamicFormQuestionComponent extends BaseWebComponent implements On
     // check custom validation
     if (this.isNumberField()) {
       if (!numberHelper.isNumber(this.getQuestionValue())) {
-        // if min value = 0 && number is 0, its all good
-        if (this.getMinNumberValue() === 0 && value === 0) {
+        // field is not a number
+        // if (min value <= 0 || max value >= 0) && number is 0, its all good
+        if ((this.getMinNumberValue() <= 0 || this.getMaxNumberValue() >= 0) && (value === 0 || !value)) {
           isValid = true;
         } else {
           isValid = false;
           errorMessageKey = 'form.error.valueIsNotANumber';
         }
       } else {
-        // field is number, but check its min & max values
+        // field is a number, but check its min & max values
         const maxValue = this.getMaxNumberValue();
         const minValue = this.getMinNumberValue();
 
-        if (value > maxValue && value > 0) {
+        if (value > maxValue) {
           isValid = false;
           errorMessageKey = 'form.error.invalidMaxNumberValue';
           translationData.number = maxValue;
         }
-        if (value < minValue && value < 0) {
+        if (value < minValue) {
           isValid = false;
           errorMessageKey = 'form.error.invalidMinNumberValue';
           translationData.number = minValue;
@@ -355,7 +356,7 @@ export class DynamicFormQuestionComponent extends BaseWebComponent implements On
       return 0;
     }
     if (definedNumber) {
-      if (definedNumber > this.miniumNumberValue) {
+      if (definedNumber < this.miniumNumberValue) {
         return this.miniumNumberValue;
       }
       return definedNumber;
