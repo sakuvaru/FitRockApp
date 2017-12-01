78721f5fde4ceeecddf364cb16dec641a33a70fb
Enngage <ennspassion@gmail.com>
2017-12-01 14:27:27 +0100

    Fix: Supports args for localization pipe

--- src/lib/localization/localization.pipe.ts
+++ src/lib/localization/localization.pipe.ts
@@ -5,19 +5,28 @@ import { LanguageResolver } from './language-resolver';
 @Pipe({
     name: 'localize',
     pure: false // required to update the value when the promise is resolved
+
 })
 export class LocalizationPipe extends TranslatePipe implements PipeTransform {
   
+    static processTranslateService(translateService: TranslateService, languageResolver: LanguageResolver): TranslateService {
+        translateService.setDefaultLang(languageResolver.getCurrentLanguage());
+        return translateService;
+    }
+
     constructor(
         private translateService: TranslateService,
         private languageResolver: LanguageResolver,
         private cdr: ChangeDetectorRef
     ) {
-        super(LocalizationPipe.getTranslateService(translateService, languageResolver), cdr);
+        super(LocalizationPipe.processTranslateService(translateService, languageResolver), cdr);
     }
 
-    static getTranslateService(translateService: TranslateService, languageResolver: LanguageResolver): TranslateService {
-        translateService.setDefaultLang(languageResolver.getCurrentLanguage());
-        return translateService;
+    transform(query: string, ...args: any[]): any {
+        let passArgs: any = {};
+        if (args.length > 0 ) {
+            passArgs = args[0];
+        }
+        return super.transform(query, passArgs);
     }
 }
