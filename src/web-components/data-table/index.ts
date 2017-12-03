315780ba85127923f962c87446988ba8fa7edc29
Enngage <ennspassion@gmail.com>
2017-12-03 16:15:11 +0100

    Feat: Improves pager component by adding a margin and exporting it

--- src/web-components/data-table/index.ts
+++ src/web-components/data-table/index.ts
@@ -4,3 +4,4 @@ export * from './data-table.module';
 export * from './data-table.config';
 export * from './data-table.interfaces';
 export * from './data-table-models';
+export * from './data-table-pager.component';
