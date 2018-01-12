7196741afe51d0c9befd6c42bf085ebf8056133e
Enngage <ennspassion@gmail.com>
2018-01-12 16:34:40 +0100

    Feat: Changes yellow color of graph

--- src/web-components/graph/graph.config.ts
+++ src/web-components/graph/graph.config.ts
@@ -46,7 +46,7 @@ export class GraphConfig<TGraph extends BaseGraph> {
      * Graph color scheme
      */
     public scheme = {
-        domain: ['#f44336', '#3f51b5', '#8b008b', '#00ced1', '#fff600', '#ffa07a']
+        domain: ['#f44336', '#3f51b5', '#8b008b', '#00ced1', '#fff500', '#ffa07a']
     };
 
     /**
