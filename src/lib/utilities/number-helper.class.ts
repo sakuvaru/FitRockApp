d8e72c45e8874be28f8bd62710b414a7cb6238da
Enngage <ennspassion@gmail.com>
2018-01-12 10:33:31 +0100

    Feat: Rounding method

--- src/lib/utilities/number-helper.class.ts
+++ src/lib/utilities/number-helper.class.ts
@@ -4,12 +4,24 @@ export class NumberHelper {
      * Returns true if given parameter is a number, false otherwise
      * @param value value to check
      */
-    isNumber(value) {
+    isNumber(value: any): boolean {
         const number = +value;
         if (number) {
             return true;
         }
         return false;
+    }
+
+    /**
+     * Rounds number to given number of decimals
+     * @param number 
+     */
+    roundTo(number: number, decimals: number): number {
+        if (!number) {
+            return 0;
+        }
+
+        return +((number).toFixed(decimals));       
     }
 }
 
