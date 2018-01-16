05bc7f986fc0366738d9f53b910c1ef44b3d231a
Enngage <ennspassion@gmail.com>
2018-01-16 08:57:17 +0100

    Updates dependencies, sets up angular template type validation, uses new datetime picker component

--- src/web-components/data-form/data-form.module.ts
+++ src/web-components/data-form/data-form.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { TextMaskModule } from 'angular2-text-mask';
-import { DateTimePickerModule } from 'ng-pick-datetime';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 
 import { ButtonsModule } from '../buttons/buttons.module';
 import { LoaderModule } from '../loader/loader.module';
@@ -11,22 +11,22 @@ import { MessagesModule } from '../messages/messages.module';
 import { SharedWebComponentModule } from '../shared-web-components.module';
 import { DataFormFieldComponent } from './data-form-field.component';
 import { DataFormComponent } from './data-form.component';
+import { MultipleChoiceDialogComponent } from './dialogs/multiple-choice-dialog.component';
 import { DataFormDeleteButtonDirective } from './directives/data-form-delete-button.directive';
 import { DataFormSaveButtonDirective } from './directives/data-form-save-button.directive';
 import { BooleanComponent } from './form-controls/boolean.component';
 import { DateTimeComponent } from './form-controls/date-time.component';
 import { DateComponent } from './form-controls/date.component';
 import { DropdownComponent } from './form-controls/dropdown.component';
+import { DurationComponent } from './form-controls/duration.component';
+import { EmailComponent } from './form-controls/email.component';
 import { HiddenComponent } from './form-controls/hidden.component';
+import { MultipleChoiceComponent } from './form-controls/multiple-choice.component';
 import { NumberComponent } from './form-controls/number.component';
 import { PhoneNumberComponent } from './form-controls/phone-number.component';
 import { RadioBooleanComponent } from './form-controls/radio-boolean.component';
 import { TextAreaComponent } from './form-controls/text-area.component';
 import { TextComponent } from './form-controls/text.component';
-import { DurationComponent } from './form-controls/duration.component';
-import { EmailComponent } from './form-controls/email.component';
-import { MultipleChoiceComponent } from './form-controls/multiple-choice.component';
-import { MultipleChoiceDialogComponent } from './dialogs/multiple-choice-dialog.component';
 
 @NgModule({
     imports: [
@@ -39,7 +39,8 @@ import { MultipleChoiceDialogComponent } from './dialogs/multiple-choice-dialog.
         ButtonsModule,
         MessagesModule,
         TextMaskModule,
-        DateTimePickerModule
+        OwlDateTimeModule,
+        OwlNativeDateTimeModule
     ],
     entryComponents: [
         MultipleChoiceDialogComponent
--- src/web-components/data-form/form-controls/date-time.component.ts
+++ src/web-components/data-form/form-controls/date-time.component.ts
@@ -1,28 +1,47 @@
 import { ChangeDetectorRef, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { DateTimeAdapter, OWL_DATE_TIME_FORMATS } from 'ng-pick-datetime';
 
 import { LocalizationService } from '../../../lib/localization';
 import { BaseFormControlComponent } from './base-form-control.component';
 
+
 @Component({
   selector: 'df-date-time',
-  templateUrl: './date-time.component.html'
+  templateUrl: './date-time.component.html',
+  providers: [
+  ],
 })
 
 export class DateTimeComponent extends BaseFormControlComponent implements OnInit, OnChanges {
 
+  private dateInitialized: boolean = false;
+
+
   constructor(
     protected cdr: ChangeDetectorRef,
-    protected localizationService: LocalizationService
+    protected localizationService: LocalizationService,
+    private dateTimeAdapter: DateTimeAdapter<any>
   ) {
     super(cdr, localizationService);
   }
 
   ngOnInit() {
     super.ngOnInit();
+    this.init();
   }
 
   ngOnChanges(changes: SimpleChanges) {
     super.ngOnChanges(changes);
+    this.init();
+  }
+
+  private init(): void {
+    if (this.dateInitialized) {
+      return;
+    }
+
+    // use locale from service (don't depend on default locale set by angular/material)
+    this.dateTimeAdapter.setLocale(this.localizationService.getLocale());
   }
 
   protected getInsertValue(): Date | string {
