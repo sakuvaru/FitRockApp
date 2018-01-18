632e741daea0505d977b7084599e410f880cc6b4
Enngage <ennspassion@gmail.com>
2018-01-18 10:00:24 +0100

    Feat: Dismissable messages

---
+++ src/web-components/messages/base/dismissable-message.component.ts
@@ -0,0 +1,64 @@
+import { Component, Input, OnChanges, SimpleChanges, OnInit } from '@angular/core';
+
+import { BaseWebComponent } from '../../base-web-component.class';
+
+@Component({
+})
+export class DismissableMessageComponent extends BaseWebComponent implements OnInit, OnChanges {
+
+    private readonly dismissStoragePrefix: string = 'dismissableMessage_';
+
+    private isDismissed: boolean = false;
+    
+    /**
+     * If set, message can be dismissed (closed). This expects an unique string
+     * so that the same string is not shown again
+     */
+    @Input() dismissCode: string;
+
+    public get showMessage(): boolean {
+        return !this.isDismissed;
+    }
+
+    public get showDismiss(): boolean {
+        return !(!this.dismissCode);
+    }
+
+    constructor(
+    ) { super();
+    }
+
+    ngOnInit(): void {
+        this.calculateDismiss();
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        this.calculateDismiss();
+    }
+
+    handleClose(): void {
+        if (!this.dismissCode) {
+            console.warn('Could not dissmiss message because no code is set');
+            return;
+        }
+        this.dismiss(this.dismissCode);
+    }
+
+    private getStorageKey(code: string): string {
+        return this.dismissStoragePrefix + code;
+    }
+
+    private dismiss(code: string): void {
+        localStorage.setItem(this.getStorageKey(code), '1');
+    }
+
+    private getIsDismissed(code: string): boolean {
+        return localStorage.getItem(this.getStorageKey(code)) === '1' ? true : false;
+    }
+
+    private calculateDismiss(): void {
+        if (this.dismissCode) {
+            this.isDismissed = this.getIsDismissed(this.dismissCode);
+        }
+    }   
+}
--- src/web-components/messages/info/info-message.component.ts
+++ src/web-components/messages/info/info-message.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 
 import { BaseWebComponent } from '../../base-web-component.class';
+import { DismissableMessageComponent } from '../base/dismissable-message.component';
 
 @Component({
     selector: 'info-message',
     templateUrl: 'info-message.component.html'
 })
-export class InfoMessageComponent extends BaseWebComponent {
+export class InfoMessageComponent extends DismissableMessageComponent {
     
     @Input() text: string;
     @Input() title: string;
