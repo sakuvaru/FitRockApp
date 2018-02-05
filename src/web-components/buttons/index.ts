1c744e7b092bff3f1e87279484c8dee972b5d6da
Enngage <ennspassion@gmail.com>
2018-02-05 09:22:53 +0100

    Fix: Buttons barrel export

--- src/web-components/buttons/index.ts
+++ src/web-components/buttons/index.ts
@@ -1,1 +1,11 @@
-export * from './button-size.enum';
+/* Action buttons */
+export * from './actions/add-button.component';
+export * from './actions/close-button.component';
+export * from './actions/delete-button.component';
+export * from './actions/edit-button.component';
+export * from './actions/fixed-button.component';
+
+/* SOcial buttons */
+export * from './social/facebook-button.component';
+export * from './social/google-button.component';
+export * from './social/twitter-button.component';
